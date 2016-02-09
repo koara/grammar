2df14b1fe552c6d4b483aa3358f821b4010bb4fb
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2016-02-09 16:57:18 +0100

    current tests lists koararenderer

--- src/test/java/io/koara/KoaraRenderer.java
+++ src/test/java/io/koara/KoaraRenderer.java
@@ -1,26 +1,73 @@
 package io.koara;
 
+import java.util.Stack;
+
 public class KoaraRenderer extends KoaraDefaultVisitor {
 
 	private StringBuffer out;
+	private int level;
+	private Stack<Integer> listSequence;
 	
 	@Override
 	public Object visit(ASTDocument node, Object data) {
 		out = new StringBuffer();
+		listSequence = new Stack<Integer>();
+		level = -1;
 		node.childrenAccept(this, data);
 		return null;
 	}
 	
 	@Override
-	public Object visit(ASTParagraph node, Object data) {
-		node.childrenAccept(this, data);
+	public Object visit(ASTHeading node, Object data) {
+		Integer headingLevel = Integer.parseInt(node.value.toString());
+		for(int i=0; i<headingLevel; i++) {
+			out.append("=");
+		}
+		if(node.children != null && node.children.length > 0) {
+			out.append(" ");
+			super.visit(node, data);
+		}
 		out.append("\n\n");
 		return null;
 	}
 	
 	@Override
-	public Object visit(ASTLineBreak node, Object data) {
+	public Object visit(ASTList node, Object data) {
+		listSequence.push(0);
+		level++;
+		super.visit(node, data);
+		level--;
+		listSequence.pop();
+		return null;
+	}
+	
+	@Override
+	public Object visit(ASTListItem node, Object data) {
+		boolean ordered = ((ASTList) node.parent).isOrdered();
+		Integer seq = (node.getNumber() != null) ? node.getNumber() : listSequence.peek() + 1;
+		out.append(indent() + (ordered ? seq + ". " : "- "));
+		level++;
+		super.visit(node, data);
+		level--;
+		return null;
+	}
+	
+	@Override
+	public Object visit(ASTParagraph node, Object data) {
+		if(node.parent.jjtGetChild(0) != node) {
+			out.append(indent());
+		}
+		node.childrenAccept(this, data);
 		out.append("\n");
+		if(!node.isSingleChild()) {
+			out.append("\n");
+		} 
+		return null;
+	}
+	
+	@Override
+	public Object visit(ASTLineBreak node, Object data) {
+		out.append("\n" + indent());
 		return null;
 	}
 	
@@ -34,6 +81,18 @@ public class KoaraRenderer extends KoaraDefaultVisitor {
 		return text.replaceAll("\\[", "\\\\[")
 				.replaceAll("\\]", "\\\\]")
 				.replaceFirst("\\=", "\\\\=");
+	}
+	
+	public String indent() {
+		if(level > 0) {
+			int repeat = level * 2;
+		    final char[] buf = new char[repeat];
+			for (int i = repeat - 1; i >= 0; i--) {
+			 buf[i] = ' ';
+			} 
+			return new String(buf);
+		}
+		return "";
 	}
 	
 	public String getOutput() {
